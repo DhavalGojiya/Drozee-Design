b913e3b133fbce9ec213a4425323f1e8e7b81266
Dhaval Gojiya <dhavalgojiya10@gmail.com>
2021-09-20 16:37:51 +0530

    init

---
+++ app.js
@@ -0,0 +1,5 @@
+let showMenu = true;
+
+document.querySelector(".nav-btn").addEventListener("click", function () {
+  document.querySelector(".navbar-slider").classList.toggle("nav_display_none");
+});
