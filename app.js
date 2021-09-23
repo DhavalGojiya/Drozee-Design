a54debd4147591067893d7518adc4e19e0a6ebd5
Dhaval <dhavalgojiya10@gmail.com>
2021-09-23 14:40:50 +0530

    toggle btn setup

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ let showMenu = true;
 document.querySelector(".nav-btn").addEventListener("click", function () {
   document.querySelector(".navbar-slider").classList.toggle("nav_display_none");
 });
+
+// navbar slider display none Javascript
+document
+  .querySelector(".close-wrapper-btn")
+  .addEventListener("click", function () {
+    document
+      .querySelector(".navbar-slider")
+      .classList.toggle("slider_display_none");
+  });
